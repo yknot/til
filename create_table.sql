11367ca2b66ebdc967fc5f6fd05ba395d8e80f0c
Andrew Yale <a.yale9@gmail.com>
2017-04-28 14:26:19 -0400

    script to create table

---
+++ create_table.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS public.posts;
+
+BEGIN;
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    developer_id INTEGER NOT NULL,
+    body TEXT NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    title character varying NOT NULL,
+    slug character varying NOT NULL,
+    likes integer DEFAULT 1 NOT NULL,
+    tweeted boolean DEFAULT false NOT NULL,
+    published_at timestamp with time zone,
+    CONSTRAINT likes CHECK ((likes >= 0))
+);
+
+COMMIT;
+
+
+INSERT INTO public.posts (
+   developer_id, body, created_at, 
+    updated_at, title, slug, likes, tweeted, 
+    published_at
+) 
+VALUES 
+    (
+        1, 'test post', now(), now(), 
+        'test', 'test', 1, false, now()
+    ),
+	(  
+        1, 'test second post', now(), now(), 
+        'again', 'test', 1, false, now()
+    );
